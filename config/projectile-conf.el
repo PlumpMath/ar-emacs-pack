bbf0d88b1de977bcacfccc157cc9b19ed06a1e4c
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-03-05 20:16:47 +0100

    Lib updated, removed f.el and s.el according to projectile's new commits.

--- config/projectile-conf.el
+++ config/projectile-conf.el
@@ -1,10 +1,6 @@
-(live-add-pack-lib "f.el")
-(live-add-pack-lib "s.el")
 (live-add-pack-lib "dash.el")
 (live-add-pack-lib "projectile")
 
-(require 'f)
-(require 's)
 (require 'dash)
 (require 'projectile)
 
