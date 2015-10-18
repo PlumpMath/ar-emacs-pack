9cbfeca17dc21ecbde16a33095831cda270139a5
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-10-18 15:14:33 -0700

    Bind figwheel repl utility function (sidecar...) on C-c C-f.

--- config/cider-conf.el
+++ config/cider-conf.el
@@ -10,9 +10,10 @@
 (setq cider-repl-history-size 1000)
 
 ;; Known hosts
-(setq cider-known-endpoints '(("localhost" "5055")))
+(setq cider-known-endpoints '(("localhost" "5055") ("localhost" "5088")))
 
 (add-hook 'cider-repl-mode-hook #'subword-mode)
+(add-hook 'cider-mode-hook #'subword-mode)
 
 ;; ac-cider
 (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
@@ -43,5 +44,28 @@
     (insert "(user/reset)")
     (cider-repl-return)))
 
+(defun cider-figwheel-repl ()
+  (interactive)
+  (save-some-buffers)
+  (with-current-buffer (cider-current-repl-buffer)
+    (goto-char (point-max))
+    (insert "(require 'figwheel-sidecar.repl-api)
+             (figwheel-sidecar.repl-api/cljs-repl)")
+    (cider-repl-return)))
+
 (global-set-key (kbd "M-s-r") 'cider-refresh)
 (global-set-key (kbd "s-r") 'cider-repl-reset)
+(global-set-key (kbd "C-c C-f") 'cider-figwheel-repl)
+
+;; From http://emacsredux.com/blog/2013/09/25/removing-key-bindings-from-minor-mode-keymaps/
+
+(defun custom-cider-mode-hook ()
+  (let ((oldmap (cdr (assoc 'cider-mode minor-mode-map-alist)))
+        (newmap (make-sparse-keymap)))
+    (set-keymap-parent newmap oldmap)
+    (define-key newmap (kbd "C-c C-z") nil)
+    (define-key newmap (kbd "C-c C-a") 'cider-switch-to-repl-buffer)
+    (make-local-variable 'minor-mode-overriding-map-alist)
+    (push `(cider-mode . ,newmap) minor-mode-overriding-map-alist)))
+
+(add-hook 'cider-mode-hook 'custom-cider-mode-hook)
