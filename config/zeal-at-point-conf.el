485999aea6c05092f8c15a933b124ec16db1653e
Andrea Richiardi <a.richiardi.work@gmail.com>
2017-02-02 09:40:04 -0800

    Update Zeal and setup right docsets for modes

--- config/zeal-at-point-conf.el
+++ config/zeal-at-point-conf.el
@@ -1,8 +1,5 @@
 (live-add-pack-lib "zeal-at-point")
 (require 'zeal-at-point)
 
-(add-to-list 'zeal-at-point-mode-alist '(emacs-lisp-mode . "emacs lisp"))
-
-;; AR - I prefer ClojureScript docs ;)
-;; (add-to-list 'zeal-at-point-mode-alist '(clojure-mode . "clojure"))
-(add-to-list 'zeal-at-point-mode-alist '(clojure-mode . "clojurescript"))
+(add-to-list 'zeal-at-point-mode-alist '(clojure-mode . "java"))
+(add-to-list 'zeal-at-point-mode-alist '(clojurescript-mode . ("clojurescript" "svg" "nodejs")))
